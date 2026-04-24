3b79d9bf4244326629a02c456145c582561237fa
dynten <hellgrea@gmail.com>
2026-04-24 11:55:16 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,17 +1,16 @@
--- node.lua – STEG 4: isolera font-laddning
+-- node.lua -- STEG 5: isolera bildladdning
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
-
-local font_ok = false
+local font = resource.load_font("font.ttf")
+local img_ok = false
 pcall(function()
-    local f = resource.load_font("font.ttf")
-    if f then font_ok = true end
+    local img = resource.load_image("background.jpg")
+    if img then img_ok = true end
 end)
 
 function node.render()
-    if font_ok then
-        gl.clear(0, 1, 0, 1)   -- GRÖN  = font.ttf laddades OK
+    if img_ok then
+        gl.clear(0, 1, 0, 1)   -- GRON  = background.jpg laddades OK
     else
-        gl.clear(1, 0.5, 0, 1) -- ORANGE = font.ttf misslyckades
+        gl.clear(1, 0.5, 0, 1) -- ORANGE = background.jpg misslyckades
     end
 end
-
