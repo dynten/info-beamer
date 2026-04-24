c6d561883720582ae7ba4d67b4850dd92b3cf064
dynten <hellgrea@gmail.com>
2026-04-24 14:02:35 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -96,5 +96,5 @@ function node.render()
     local text_size = ticker_h * 0.7
     local text_w = font:width(ticker_text, text_size)
     if ticker_x < -text_w then ticker_x = vw end
-    font:write(ticker_x, content_h + (ticker_h - text_size) / 2, ticker_text, text_size, 1, 1, 1, 1)
+    font:write(ticker_x, content_h + (ticker_h - text_size) / 2, ticker_text, text_size, 0.05, 0.1, 0.35, 1)
 end
